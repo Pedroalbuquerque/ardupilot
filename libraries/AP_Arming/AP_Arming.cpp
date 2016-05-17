62b811a452ee22229042755b1bb63ae9b5f51e61
Pedro Albuquerque <pedro_m_albuquerque@hotmail.com>
2016-05-17 18:18:20 +0100

    Merge remote-tracking branch 'ArduPilot/master'
    # Conflicts:
    #	libraries/AP_Arming/AP_Arming.cpp

--- libraries/AP_Arming/AP_Arming.cpp
+++ libraries/AP_Arming/AP_Arming.cpp
@@ -210,9 +210,18 @@ bool AP_Arming::ins_checks(bool report)
                      */
                     threshold *= 3;
                 }
+<<<<<<< HEAD
                 // EKF is less sensitive to Z-axis error and Z-axis is more
                 // likely to be temperature-sensitive on our hardware
                 vec_diff.z *= 0.5f;
+=======
+
+                // EKF is less sensitive to Z-axis error and Z-axis is more
+                // likely to be temperature-sensitive on MEMS accel sensors
+                // such as the MPU-6000.
+                vec_diff.z *= 0.5f;
+
+>>>>>>> ArduPilot/master
                 if (vec_diff.length() <= threshold) {
                     last_accel_pass_ms[i] = AP_HAL::millis();
                 }
